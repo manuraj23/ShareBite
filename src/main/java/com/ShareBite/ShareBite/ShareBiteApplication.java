3b16d28cb2d7eff73b68a3abbb7a843861f3c5d2
manuraj23 <manuraj082004@gmail.com>
2025-09-23 15:49:30 +0530

    Basic Files added

---
+++ src/main/java/com/ShareBite/ShareBite/ShareBiteApplication.java
@@ -0,0 +1,13 @@
+package com.ShareBite.ShareBite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareBiteApplication.class, args);
+	}
+
+}
